6f97e2236fabef2e59401846cb84fec3a72f7d7d
Ankit Ojha <ankitojha1409@gmail.com>
2023-12-04 09:08:08 +0530

    Dark mode and light mode buttons added

--- app/src/main/java/com/ankitojha/testapp/MainActivity.kt
+++ app/src/main/java/com/ankitojha/testapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ankitojha.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLight = findViewById<Button>(R.id.btnLight)
+        val btnDark = findViewById<Button>(R.id.btnDark)
+
+        btnDark.setOnClickListener{
+            Toast.makeText(applicationContext, "Uploading...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
