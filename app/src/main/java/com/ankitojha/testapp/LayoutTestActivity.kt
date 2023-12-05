4d54431ca15d0e8c771c3bf570c5dc4162a0286a
Ankit Ojha <ankitojha1409@gmail.com>
2023-12-05 10:55:42 +0530

    Layout testing started

---
+++ app/src/main/java/com/ankitojha/testapp/LayoutTestActivity.kt
@@ -0,0 +1,11 @@
+package com.ankitojha.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_test)
+    }
+}
